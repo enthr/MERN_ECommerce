ac3e1b6e56982d64aac7ff8067497a2a51bf64c0
Jaimin Prajapati <enthralall@gmail.com>
2024-01-02 00:45:31 +0530

    Add Header and Footer To Root Layout

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -3,6 +3,8 @@ import { Outlet } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import { lightTheme, darkTheme } from '../lib/theme';
 
 const Root = () => {
@@ -10,7 +12,9 @@ const Root = () => {
 	return (
 		<ThemeProvider theme={(darkMode) ? darkTheme : lightTheme}>
             <CssBaseline />
+			<Header darkMode={darkMode} setDarkMode={setDarkMode} />
             <Outlet />
+			<Footer />
 		</ThemeProvider>
 	);
 };
