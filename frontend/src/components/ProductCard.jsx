803272018eb74e07c2c1a12ce9cc9471afe04390
Jaimin Prajapati <enthralall@gmail.com>
2024-01-11 13:22:37 +0530

    Refactor ProductCard component to use dynamic image source.

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,5 +1,4 @@
 import { Link as RouterLink } from 'react-router-dom';
-
 import Stack from '@mui/material/Stack';
 import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
@@ -10,19 +9,23 @@ import Rating from '@mui/material/Rating';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
+import { SERVER } from '../lib/constants';
+
 const Product = ({ product }) => {
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
+    const imageSrc = (product.image.includes('images')) ? `${SERVER}/uploads${product.image}` : `${SERVER}/${product.image}`;
+
 	return (
 		<Link component={RouterLink} to={`/product/${product._id}`} color="inherit" underline="none">
-			<Card raised={true}>
-				<CardMedia component="img" image={product.image} alt={product.name} sx={{ paddingX: 3, paddingTop: 3, paddingBottom: 1 }} />
-				<CardContent sx={{ paddingX: 3 }}>
-					<Typography noWrap={(isMobile) ? false : true} variant="h5" component="h5" fontSize="1.4rem" fontWeight="500">
+			<Card variant="elevation" raised={true}>
+				<CardMedia component="img" image={imageSrc} alt={product.name} height="300px" />
+				<CardContent sx={{ marginX: 3, marginY: 2 }}>
+					<Typography noWrap={(isMobile) ? false : true} variant="h5" component="h5" fontWeight="500">
 						{product.name}
 					</Typography>
-					<Stack direction="row" alignItems="center" spacing={1} marginY={1}>
+					<Stack direction="row" alignItems="center" spacing={1} marginY={2}>
 						<Rating name={`${product._id}_rating`} size="large" value={product.rating} precision={0.25} readOnly />
                         <Typography variant="h6" component="h6" color="text.secondary" fontWeight={500}>
                             {product.numReviews} Reviews
