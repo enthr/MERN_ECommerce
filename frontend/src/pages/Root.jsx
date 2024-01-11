a6062a8b37427dc011786b6cbfbad53bbb2890b4
Jaimin Prajapati <enthralall@gmail.com>
2024-01-11 13:28:23 +0530

    Refactor Root component to use Redux for dark mode and add ToastContainer

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -1,20 +1,25 @@
-import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import { ToastContainer } from 'react-toastify';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { lightTheme, darkTheme } from '../lib/theme';
 
+import 'react-toastify/dist/ReactToastify.min.css';
+
 const Root = () => {
-    const [darkMode, setDarkMode] = useState(false);
+    const darkMode = useSelector((state) => state.theme.darkMode);
+
 	return (
-		<ThemeProvider theme={(darkMode) ? darkTheme : lightTheme}>
-            <CssBaseline />
-			<Header darkMode={darkMode} setDarkMode={setDarkMode} />
-            <Outlet />
+		<ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+			<CssBaseline />
+			<Header />
+			<Outlet />
 			<Footer />
+			<ToastContainer position="bottom-right" theme='dark' pauseOnHover autoClose={5000} />
 		</ThemeProvider>
 	);
 };
