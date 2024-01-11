a1e221088285751507ad7fbd64acba0e243110ba
Jaimin Prajapati <enthralall@gmail.com>
2024-01-11 13:32:56 +0530

    Add calculatePrices utility function.

---
+++ backend/src/utils/calculatePrices.js
@@ -0,0 +1,18 @@
+function addDecimals(num) {
+    return (Math.round(num * 100) / 100).toFixed(2);
+}
+
+export function calculatePrices(orderItems) {
+    const itemsPrice = orderItems.reduce((acc, item) => acc + (item.price * 100 * item.qty) / 100, 0);
+
+    const shippingPrice = itemsPrice > 100 ? 0 : 10;
+    const taxPrice = 0.18 * itemsPrice;
+    const totalPrice = itemsPrice + shippingPrice + taxPrice;
+
+    return {
+        itemsPrice: addDecimals(itemsPrice),
+        shippingPrice: addDecimals(shippingPrice),
+        taxPrice: addDecimals(taxPrice),
+        totalPrice: addDecimals(totalPrice),
+    };
+}
